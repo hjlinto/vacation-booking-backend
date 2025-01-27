3444e2bbf47e956edf4a3077b4a89ec3f9718c8b
Hunter Linton <hlinto2@wgu.edu>
2025-01-27 14:03:33 -0800

    Add entity classes and enums to match UML diagram

---
+++ src/main/java/com/example/demo/entities/Division.java
@@ -0,0 +1,51 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "divisions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Division {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "division_id", nullable = false)
+    private Long id;
+
+    @Column(name = "division", nullable = false)
+    private String division_name;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private Date createDate;
+
+    @Column(name = "last_update", updatable = false)
+    @UpdateTimestamp
+    private Date lastUpdate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false, insertable = false, updatable = false)
+    private Country country;
+
+    @Column(name = "country_id", nullable = false)
+    private Long country_id;
+
+    public void setCountry(Country country) {
+        setCountry_id(country.getId());
+        this.country = country;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "division")
+    private Set<Customer> customers = new HashSet<>();
+}
